6dc5cc6ed5464b1bfc1035c489e9ed4d22d83f1b
Nischaya Sharma <nischaya@Apples-MacBook-Air.local>
2018-12-08 10:13:42 +0530

    feat: Solution to Triangle words and restructing directoris for keeing files.

--- src/TriangleWords.py
+++ src/TriangleWords.py
@@ -1,9 +1,22 @@
 import os;
 
-triangleNums = [];
-for i in range(2000):
-    triangleNums.append((i*(i+1))/(2));
-with open(os.path.dirname(os.path.realpath(__file__))+"/p042_words.txt","r") as fo:
+with open("Users/nischaya/Documents/project-euler/files/p042_words.txt","r") as fo:
     words = fo.readlines();
 words = words[0].split(",");
-print words;
+counter = 0;
+isTriangleWord = False;
+for word in words:
+    sum = 0;
+    i = 0;
+    for letter in word:
+        sum += ord(letter)-64;
+    while True:
+        num = (i*(i+1))/(2);
+        if(sum==num):
+            isTriangleWord = True;
+            counter += 1;
+            break;
+        if(num>sum):
+            break;
+        i+=1;
+print counter;
