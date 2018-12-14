db64ab6dd61825263f420e74498618465f32597e
Nischaya Sharma <nischaya@Nischayas-MacBook-Air.local>
2018-12-14 12:46:58 +0530

    fix: Partial solution to Consecutive Prime sum roblem.

--- src/ConsPrimeSum.py
+++ src/ConsPrimeSum.py
@@ -11,10 +11,10 @@ def isPrime(num):
     return True;
 
 sum = 0;
-for i in range(1,101):
+for i in range(1,1001):
     sum = 0;
     if(isPrime(i)):
-        for j in range(1,i):
+        for j in range(i,1,-1):
             if(isPrime(j)):
                 sum+=j;
                 if(sum==i):
